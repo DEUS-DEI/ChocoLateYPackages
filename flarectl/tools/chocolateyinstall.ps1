bd2b1a7ca8d9df558f32e4bd9f2ec2c5f431d4c9
DEUS-DEI <16824459+DEUS-DEI@users.noreply.github.com>
2022-07-21 17:48:11 -0500

    Fix
    Fix Arguments

--- flarectl/tools/chocolateyinstall.ps1
+++ flarectl/tools/chocolateyinstall.ps1
@@ -1,17 +1,10 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$url64      = 'https://github.com/cloudflare/cloudflare-go/releases/download/v0.45.0/flarectl_0.45.0_windows_amd64.zip' 
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-      FileFullPath  = Join-Path $toolsDir "flarectl.exe"
-  url64bit      = $url64
-  softwareName  = 'flarectl*' 
+  url64bit      = 'https://github.com/cloudflare/cloudflare-go/releases/download/v0.45.0/flarectl_0.45.0_windows_amd64.zip'
   checksum64    = '9f31e7ab673bc70cc897d3d5d29e0301d82b98f6aa8a53259ac2451231550c9b'
-  checksumType64= 'sha256' 
- 
+  checksumType64= 'sha256'
 }
-
-Get-ChocolateyWebFile @packageArgs
+Install-ChocolateyZipPackage @packageArgs
