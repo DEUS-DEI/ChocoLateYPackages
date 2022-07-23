86a2da4e3c91c0d9c6a17cf66c25cc6921de5847
DEUS-DEI <16824459+DEUS-DEI@users.noreply.github.com>
2022-07-23 15:52:37 -0500

    Add Package FWS & FWSB

---
+++ fenix-web-server-beta/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName= 'fenix-web-server-beta'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/coreybutler/fenix/releases/download/3.0.0-rc.13/Fenix.Setup.3.0.0-rc.13.exe'
+$FileLocation = Join-Path $toolsDir 'fenix-web-server-beta.exe'
+Get-ChocolateyWebFile -PackageName 'fenix-web-server-beta' `
+                      -Url $url -FileFullPath $FileLocation `
+                      -Checksum '6f2ca055f95a181ea2d9a133a31c1d9b881e894e7c57780a6a3dc529a54e076e' `
+                      -ChecksumType 'sha256'
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  softwareName  = 'Fenix*'
+}
+Install-ChocolateyInstallPackage @packageArgs
---
+++ fenix-web-server/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName= 'fenix-web-server'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/coreybutler/fenix/releases/download/v2.0.0/fenix-windows-2.0.0.zip'
+$FileLocation = Join-Path $toolsDir 'fenix-web-server.zip'
+Get-ChocolateyWebFile -PackageName 'fenix-web-server' `
+                      -Url $url -FileFullPath $FileLocation `
+                      -Checksum '9b4871180f912464b6683f8bdd843184df58c0e6f970703c304334fd5ddca24e' `
+                      -ChecksumType 'sha256'
+
+Get-ChocolateyUnzip -FileFullPath $FileLocation -Destination $toolsDir
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname
+  silentArgs    = ""
+  validExitCodes= @(0)
+  softwareName  = 'Fenix*'
+}
+Install-ChocolateyInstallPackage @packageArgs
