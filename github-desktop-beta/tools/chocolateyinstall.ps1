bdfa29029f590a1b405b81fac5712d2908dde806
DEUS-DEI <16824459+DEUS-DEI@users.noreply.github.com>
2022-07-28 12:53:42 -0500

    Github 3.0.6-beta2

--- github-desktop-beta/tools/chocolateyinstall.ps1
+++ github-desktop-beta/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName= 'github-desktop-beta'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://desktop.githubusercontent.com/github-desktop/releases/3.0.6-beta1-0ef901ea/GitHubDesktopSetup-x64.exe'
+$url = 'https://desktop.githubusercontent.com/github-desktop/releases/3.0.6-beta2-706ecf57/GitHubDesktopSetup-x64.exe'
 $FileLocation = Join-Path $toolsDir 'github-desktop-beta.exe'
 Get-ChocolateyWebFile -PackageName 'github-desktop-beta' `
                       -Url $url -FileFullPath $FileLocation `
-                      -Checksum '1035f6ef8a1a99f21f539d5e9b16ae49f791518647c7e3f4f4ba68cc633612b6' -ChecksumType 'sha256'
+                      -Checksum '9d03150cc9ce518f9ebe655050761ae06834b3a8959b0c898395abcb22038e11' -ChecksumType 'sha256'
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
