2b777dab307a7cd43db6d315182e679181dedaa3
DEUS-DEI <16824459+DEUS-DEI@users.noreply.github.com>
2022-08-03 00:26:55 -0500

    flarectl 0.46.0

--- flarectl/tools/chocolateyinstall.ps1
+++ flarectl/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url64bit      = 'https://github.com/cloudflare/cloudflare-go/releases/download/v0.45.0/flarectl_0.45.0_windows_amd64.zip'
-  checksum64    = '9f31e7ab673bc70cc897d3d5d29e0301d82b98f6aa8a53259ac2451231550c9b'
+  url64bit      = 'https://github.com/cloudflare/cloudflare-go/releases/download/v0.46.0/flarectl_0.46.0_windows_amd64.zip'
+  checksum64    = 'eb3e5b2aefb232b72b35c336fd14913d60beba8693707e9b3d11466222423507'
   checksumType64= 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
