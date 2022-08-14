ae23e32eca68f1cd1b95816eb8e18e6db64602d3
DEUS-DEI <16824459+DEUS-DEI@users.noreply.github.com>
2022-08-14 01:09:33 -0500

    Add Thunderbird Beta & change bugTrackerUrl in Daily

---
+++ thunderbird-beta/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName= 'thunderbird-beta'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/104.0b3/win64/en-US/Thunderbird%20Setup%20104.0b3.exe'
+$FileLocation = Join-Path $toolsDir 'thunderbird-beta.exe'
+Get-ChocolateyWebFile -PackageName 'thunderbird-beta' `
+                      -Url $url -FileFullPath $FileLocation `
+                      -Checksum 'c6a9629f4e6c5297bf22476e5f3ae41ff55b292e90ff1adaa28bf5a73adca8ed' `
+                      -ChecksumType 'sha256'
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname
+  silentArgs    = "-ms"
+  validExitCodes= @(0)
+  softwareName  = 'Mozilla Thunderbird*'
+}
+Install-ChocolateyInstallPackage @packageArgs
---
+++ thunderbird-beta/tools/chocolateyuninstall.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName    = 'thunderbird-beta'
+$softwareName   = 'Mozilla Thunderbird*'
+$fileType       = 'exe'
+$silentArgs     = '"%ProgramFiles%\Mozilla Thunderbird Beta\uninstall\helper.exe" /S'
+$validExitCodes = @(0)
+[array] $key = Get-UninstallRegistryKey -SoftwareName $softwareName
+if ($key.Count -eq 1) {
+    $key | % { 
+        if ($_.UninstallString) {
+            function Split-CommandLine {
+                param([string] $file)
+                return $file
+            }
+            $file = Invoke-Expression "Split-CommandLine $($_.UninstallString)"
+        }
+        if ($file -and (Test-Path $file)) {
+            Uninstall-ChocolateyPackage -PackageName $packageName `
+                                        -FileType $fileType `
+                                        -SilentArgs $silentArgs `
+                                        -ValidExitCodes $validExitCodes `
+                                        -File $file
+        } else {
+            Write-Warning "$packageName has already been uninstalled by other means. Unknown uninstaller: $file ($($_.UninstallString))."
+        }
+    }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert the package maintainer that the following keys were matched:"
+  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
+}
