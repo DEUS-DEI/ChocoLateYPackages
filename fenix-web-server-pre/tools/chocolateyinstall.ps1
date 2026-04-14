c9c8144ca4ef29c42e23852d752f5af2ba875aa3
DEUS-DEI <16824459+DEUS-DEI@users.noreply.github.com>
2026-04-14 08:13:32 -0600

    chore: unify packageName variable usage across all scripts

--- fenix-web-server-pre/tools/chocolateyinstall.ps1
+++ fenix-web-server-pre/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $packageName= 'fenix-web-server-pre'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://github.com/coreybutler/fenix/releases/download/3.0.0-rc.13/Fenix.Setup.3.0.0-rc.13.exe'
-$FileLocation = Join-Path $toolsDir 'fenix-web-server-beta.exe'
-Get-ChocolateyWebFile -PackageName 'fenix-web-server-beta' `
+$FileLocation = Join-Path $toolsDir "$packageName.exe"
+Get-ChocolateyWebFile -PackageName $packageName `
                       -Url $url -FileFullPath $FileLocation `
                       -Checksum '6f2ca055f95a181ea2d9a133a31c1d9b881e894e7c57780a6a3dc529a54e076e' `
                       -ChecksumType 'sha256'
--- github-desktop-pre/tools/chocolateyinstall.ps1
+++ github-desktop-pre/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $packageName= 'github-desktop-pre'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://desktop.githubusercontent.com/releases/3.5.7-c5e06544/GitHubDesktopSetup-x64.exe'
-$FileLocation = Join-Path $toolsDir 'github-desktop-beta.exe'
-Get-ChocolateyWebFile -PackageName 'github-desktop-beta' `
+$FileLocation = Join-Path $toolsDir "$packageName.exe"
+Get-ChocolateyWebFile -PackageName $packageName `
                       -Url $url -FileFullPath $FileLocation `
                       -Checksum '9d03150cc9ce518f9ebe655050761ae06834b3a8959b0c898395abcb22038e11' -ChecksumType 'sha256'
 $packageArgs = @{
